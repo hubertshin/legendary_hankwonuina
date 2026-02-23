559534d64729f9537dc803cac73e0c2f8888c23a
Hubert Shin <hubertshin@users.noreply.github.com>
2026-02-23 15:02:25 +0900

    Add MetaPixelInit component for initializing Meta Pixel

---
+++ src/components/MetaPixelInit.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useEffect } from 'react';
+import { initPixel } from '@/lib/metaPixel';
+
+export function MetaPixelInit() {
+  useEffect(() => {
+    const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+    if (pixelId) initPixel(pixelId);
+  }, []);
+
+  return null;
+}
