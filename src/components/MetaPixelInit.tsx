c8396cb8ab5dc2cfb772d445a8d3f5d8208d1c2a
Hubert Shin <hubertshin@users.noreply.github.com>
2026-02-23 17:51:43 +0900

    Set Meta Pixel ID directly in MetaPixelInit

--- src/components/MetaPixelInit.tsx
+++ src/components/MetaPixelInit.tsx
@@ -5,7 +5,7 @@ import { initPixel } from '@/lib/metaPixel';
 
 export function MetaPixelInit() {
   useEffect(() => {
-    const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+    const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID=1151930330177738;
     if (pixelId) initPixel(pixelId);
   }, []); 
 
